a9d2166f6b8e9104c8fb4edd4d22ab343b499f60
Rien Bijl <helloitsrien@gmail.com>
2019-12-13 11:18:54 +0100

    :art: Gandalfified

---
+++ www/index.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ *                        ,---.
+                       /    |
+                      /     |
+  Gandalf            /      |
+                    /       |
+               ___,'        |
+             <  -'          :
+              `-.__..--'``-,_\_
+                 |o/ ` :,.)_`>
+                 :/ `     ||/)
+                 (_.).__,-` |\
+                 /( `.``   `| :
+                 \'`-.)  `  ; ;
+                 | `       /-<
+                 |     `  /   `.
+ ,-_-..____     /|  `    :__..-'\
+/,'-.__\\  ``-./ :`      ;       \
+`\ `\  `\\  \ :  (   `  /  ,   `. \
+  \` \   \\   |  | `   :  :     .\ \
+   \ `\_  ))  :  ;     |  |      ): :
+  (`-.-'\ ||  |\ \   ` ;  ;       | |
+   \-_   `;;._   ( `  /  /_       | |
+    `-.-.// ,'`-._\__/_,'         ; |
+       \:: :     /     `     ,   /  |
+        || |    (        ,' /   /   |
+        ||                ,'   / SSt|
+
+
+This is Gandalf, he is a vagabond that wanders the world (don't tell him that) and he will now mark his
+footprints through this wonderful application. On the way he will perform magic beyond comprehension and will
+perform miracles that would shatter the mind of a mere mortal. Best not to mess with this file, or he will be
+very upset!
+ */
