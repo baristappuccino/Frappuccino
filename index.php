f3c8c47e340077c0b7620812a2e39d7bc58d20c0
Rien Bijl <helloitsrien@gmail.com>
2019-12-13 17:05:13 +0100

    Slight refactor and added route override

--- index.php
+++ index.php
@@ -31,10 +31,12 @@
 
 This is Gandalf, he is a vagabond that wanders the world (don't tell him that) and he will now mark his
 footprints through this wonderful application. On the way he will perform magic beyond comprehension and will
-perform miracles that would shatter the mind of a mere mortal. Best not to mess with this file, or he will be
-very upset!
+perform miracles that would shatter the mind of a mere mortal. You are not to change this file in any way, for
+you should not take him as a conjurer performing cheap tricks. Pass and do not look back at this file, for you
+will feel his wrath if you do!
  */
 
+define("FRAPPUCINO_START", microtime(true));
 $request = \Kernel\Request::extract();
 
 /**
